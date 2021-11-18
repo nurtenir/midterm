3abaeb9bb053d5c538426f69b6280d3f37eb3e57
nurtenir <92497598+nurtenir@users.noreply.github.com>
2021-11-18 13:04:06 +0600

    Create indexx.php

---
+++ indexx.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nurtenir</title>
+</head>
+<body>
+    <h1>My summary</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>Anarbek uulu Nurtenir</h1>
+
+body {
+  background-image: backgroun.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+nav {
+  float: inline-start;
+  width: 100%;
+  height: 100px;
+  padding: 5px;
+}
+
+
+
+</body>
+</html>
+
+
+</head>
+<body>
+	                           <img src="nuru.jpg"> 
+	                        
+<body>
+</html>
+
+</head>
+<body>
+	  <a href="https://web.whatsapp.com/">WhatsApp</a>
+	 <a href="https://www.instagram.com/nuru___a/">Instagram</a>
+	 <a href="https://web.telegram.org/z/">Telegram</a>
+	
+<body>
+</html>	
+	
+</head>
+<body>
+    <h1>23.06.2004</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My phone number;0502484857</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My adress: Aaly Tokombaeva 27</h1>
+</body>
+</html>
+
+
+</head>
+<body>
+    <h1>i am senior collage student at it bussines college in International Ala-Too university </h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My department is Computer system and comploxes</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>Next year i am planning to apply MIT</h1>
+</body>
+</html>
